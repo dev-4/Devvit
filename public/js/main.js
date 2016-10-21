3562615304973956b1490c182553035f5cd7e5d8
dev-4 <dev.4@protonmail.com>
2016-10-21 14:46:19 -0400

    Fixed javascript function

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,24 @@
 var elMessage, elMessageCharCount, elConsole;
 
 $(document).ready(function(){
-  startTime();
   elMessage = document.querySelector("textarea[name=message]");
   elMessageCharCount = document.getElementById("message-remaining");
   elConsole = document.getElementById("console");
   // Character Count update
   updateRemaining(elMessage,elMessageCharCount);
 
-  elMessage.addEventListener("keydown",function(event){
+  elMessage.addEventListener("keyup",function(event){
     // if there's still room to write, update the remaining character count and let them go through
 
     // console.log( elMessage, elMessageCharCount );
     updateRemaining( elMessage, elMessageCharCount );
     // else, if there's no room, block the letters
   });
+}); // $(document).ready
 
-  // Takes a number of characters and updates element
+// Takes a number of characters and updates element
 function updateRemaining( elInput, elMsg ) {
-  var remaining = elInput.getAttribute('maxlength') - elInput.value.length;
+  var remaining = parseInt(elInput.getAttribute('maxlength')) - elInput.value.length;
   if( remaining >= 0 ) {
     elMsg.innerText = remaining;
     return true;
